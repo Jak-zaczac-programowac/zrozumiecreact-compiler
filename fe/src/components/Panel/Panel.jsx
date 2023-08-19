b43d36f3759aea79f4bdee583664d174157185c9
filipMaM <filip.mamcarczyk@gmail.com>
2023-08-19 16:42:34 +0200

    Add Form

--- fe/src/components/Panel/Panel.jsx
+++ fe/src/components/Panel/Panel.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Button } from "../Button/Button";
 import { List } from "../List/List";
 import styles from "./Panel.module.css";
+import { Form } from "../Form/Form";
 
 export function Panel() {
     const [data, setData] = useState([
@@ -29,6 +30,7 @@ export function Panel() {
         <>
             <Button>Załaduje dane</Button>
             <section className={styles.section}>
+                <Form />
                 <List data={data}></List>
             </section>
         </>
