c958d709c46410e783294812c3de4ee14cc6c181
filipMaM <filip.mamcarczyk@gmail.com>
2023-08-19 16:21:19 +0200

    Revert "Lesson 1 end"
    This reverts commit 4d901e1bc3ddc34b33e5f4f73980430f3d893cfd.

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,20 +1,10 @@
-import { useState } from "react";
 import "./App.module.css";
 import { Panel } from "./components/Panel/Panel";
-import { Button } from "./components/Button/Button";
 
 function App() {
-    const [isPanelShown, setIsPanelShown] = useState(true);
     return (
         <>
-            <Button
-                onClick={() =>
-                    setIsPanelShown((prevIsPanelShown) => !prevIsPanelShown)
-                }
-            >
-                {isPanelShown ? "Schowaj panel" : "Pokaż panel"}
-            </Button>
-            {isPanelShown && <Panel />}
+            <Panel></Panel>
         </>
     );
 }
